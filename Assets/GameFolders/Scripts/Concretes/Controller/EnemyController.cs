6aca50ab174fc51e67bc1bf7811de5d2675f9e78
Emre Hün <the.emrehun@gmail.com>
2023-06-14 09:20:34 +0300

    Added Zombie Attack Animation
    Zombie attacks cause damage from now

--- Assets/GameFolders/Scripts/Concretes/Controller/EnemyController.cs
+++ Assets/GameFolders/Scripts/Concretes/Controller/EnemyController.cs
@@ -12,31 +12,49 @@ namespace UdemyProject3.Controller
 {
     public class EnemyController : MonoBehaviour, IEntityController
     {
-        [SerializeField] Transform _playerPrefab;
-
         IMover _mover;
         IHealth _iHealth;
         CharacterAnimation _characterAnimation;
         NavMeshAgent _navMeshAgent;
+        InventoryController _inventoryController;
+        Transform _playerTransform;
+        bool _canAttack;
         void Awake()
         {
             _mover = new MoveWithNavMesh(this);
             _characterAnimation = new CharacterAnimation(this);
             _navMeshAgent = GetComponent<NavMeshAgent>();
+            _inventoryController = GetComponent<InventoryController>();
             _iHealth = GetComponent<IHealth>();
         }
 
-        
+        void Start()
+        {
+            _playerTransform = FindObjectOfType<PlayerController>().transform;
+        }
+
+
         void Update()
         {   
             if(_iHealth.IsDead) return;
 
-            _mover.MoveAction(_playerPrefab.transform.position, 10f);
+            _mover.MoveAction(_playerTransform.position, 10f);
+
+            _canAttack = Vector3.Distance(_playerTransform.position, this.transform.position) <= _navMeshAgent.stoppingDistance && _navMeshAgent.velocity == Vector3.zero;
+        }
+
+        void FixedUpdate()
+        {
+            if(_canAttack) {
+
+                _inventoryController.CurrentWeapon.CanAttack();
+            }
         }
 
         void LateUpdate()
         {
             _characterAnimation.MoveAnimation(_navMeshAgent.velocity.magnitude);
+            _characterAnimation.AttackAnimation(_canAttack);
         }
     }
 }
