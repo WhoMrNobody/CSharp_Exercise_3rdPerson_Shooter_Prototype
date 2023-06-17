84865d537c1397d887c588638ea7bc1a8b365b3b
Emre Hün <the.emrehun@gmail.com>
2023-06-17 14:35:55 +0300

    Added and Adjust Enemy Spawner

--- Assets/GameFolders/Scripts/Concretes/Controller/SpawnerController.cs
+++ Assets/GameFolders/Scripts/Concretes/Controller/SpawnerController.cs
@@ -8,6 +8,32 @@ namespace UdemyProject3.Controller
     public class SpawnerController : MonoBehaviour
     {
         [SerializeField] SpawnInfoSO _spawnInfo;
+
+        float _maxTime;
+        float _currentTime;
+
+        void Start()
+        {
+            _maxTime = _spawnInfo.RandomSpawnMaxTime;
+        }
+
+        void Update()
+        {
+            _currentTime += Time.deltaTime;
+
+            if(_currentTime > _maxTime)
+            {
+                Spawn();
+            }
+        }
+
+        void Spawn()
+        {
+            Instantiate(_spawnInfo.EnemyPrefab, transform.position, Quaternion.identity);
+
+            _currentTime = 0f;
+            _maxTime = _spawnInfo.RandomSpawnMaxTime;
+        }
     }
 }
 
